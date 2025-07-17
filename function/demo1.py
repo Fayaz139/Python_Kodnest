920295d71fbf2bf322945538c9ae6f8eca5d753a
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2025-07-17 18:09:52 +0530

    added types in functions basics

--- function/demo1.py
+++ function/demo1.py
@@ -5,9 +5,27 @@ def greet():
 greet()
 
 # PARAMETER + NO RETURN
+def add(a , b):
+    sum = (a+b)
+    print("Sum =",sum)
 
+add(10,20)
 
 # NO PARAMETER + RETURN
+def calc_SI():
+    p = 1000
+    t = 3
+    r = 12.5
+    si = (p*t*r)/100
+    return si
 
+res = calc_SI()
+print("SI =",res)
 
 # PARAMETER + RETURN
+def find_cube(n):
+    cube = n ** 3
+    return cube
+
+result = find_cube(5)
+print("Cube =",result)
