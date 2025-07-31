0e290cee064059ecf0bc5e53ce81c909e4ff2c0e
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2025-07-31 09:29:06 +0530

    30 july 2025

---
+++ basics/list_comprehension.py
@@ -0,0 +1,5 @@
+lst = [10, 20, 30, 40, 50, 60]
+double = []
+for i in range(lst):
+    double.append(i * 2)
+print(double)
---
+++ typeOf.py
@@ -0,0 +1,17 @@
+lst = []
+print(lst)
+print(type(lst))
+
+tup = ()
+print(tup)
+print(type(tup))
+
+dict = {():()}
+print(dict)
+print(len(dict))
+print(type(dict))
+
+s = set()
+print(s)
+print(type(s))
+print(len(s))
